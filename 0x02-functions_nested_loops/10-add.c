7c4385eb3c891a7791bd1c373ba46da31a0197e0
HichamELMaa <hm.elmaataoui@gmail.com>
2023-02-21 09:55:29 -0800

    Return two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,40 +1,14 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * print_to_98 - a function that prints all natural numbers from n to 98
- * user input's number prints to 98, regardless < 98 or > 98
- * @n: number input
- * Return: Always 0 (Success)
+ * add -> a function that adds two integers and returns the result.
+ *
+ * @a: a is an integer argument
+ * @b: b is an integer argument
+ *
+ * Return: The addition of two numbers
  */
-void print_to_98(int n)
+int add(int a, int b)
 {
-	if (n < 98)
-	{
-		while (n <= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n++;
-		}
-	}
-	else if (n > 98)
-	{
-		while (n >= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n--;
-		}
-	}
-	else
-	{
-		printf("98");
-	}
-	printf("\n");
+	return (a + b);
 }

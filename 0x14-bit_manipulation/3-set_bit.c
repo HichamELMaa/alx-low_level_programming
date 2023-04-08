4a2ce8b02b860e24a7cf10b8ec48ff222905b53a
HichamELMaa <hm.elmaataoui@gmail.com>
2023-04-08 13:48:20 -0700

    value of a bit to 1 at a given index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,12 +8,12 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask;
+	unsigned long int i;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	mask = 1 << index;
-	*mask = *mask | i;
+	i = 1 << index;
+	*n = *n | i;
 
 	return (1);
 }

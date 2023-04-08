9839b6b28296bb7c31f9082f81fe0b726ef92bc0
HichamELMaa <hm.elmaataoui@gmail.com>
2023-04-08 14:07:56 -0700

    bit to 0 at a given index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,11 +9,13 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+    unsigned long int mask;
+
     if (index >= (sizeof(unsigned long int) * 8))
         return (-1);
 
-    unsigned long int mask = 1 << index;
-    *n &= ~mask;
+    mask = ~(1 << index);
+    *n &= mask;
 
     return (1);
 }

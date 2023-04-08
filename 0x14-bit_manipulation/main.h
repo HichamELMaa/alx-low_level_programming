89338efc29d3fdecb60926f0764b5a0e8468be2b
HichamELMaa <hm.elmaataoui@gmail.com>
2023-04-08 09:12:21 -0700

    function that converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-bit_manipulation directory.
+ */
+
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */

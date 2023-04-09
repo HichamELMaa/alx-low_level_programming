f343935f516f10d3c4f716664fe497aff032bd80
HichamELMaa <hm.elmaataoui@gmail.com>
2023-04-09 04:30:41 -0700

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,6 +2,7 @@
 
 /**
  * create_file - Creates a file with the specified name and content.
+ *
  * @filename: The name of the file to create.
  * @text_content: A NULL terminated string to write to the file.
  *
@@ -14,15 +15,18 @@ int create_file(const char *filename, char *text_content)
     if (filename == NULL)
         return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+    if (text_content != NULL)
+    {
+        while (text_content[len])
+            len++;
+    }
+
+    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     if (fd == -1)
         return (-1);
 
     if (text_content != NULL)
     {
-        while (text_content[len])
-            len++;
-
         if (write(fd, text_content, len) != len)
         {
             close(fd);
@@ -33,3 +37,4 @@ int create_file(const char *filename, char *text_content)
     close(fd);
     return (1);
 }
+

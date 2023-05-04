3ab89c69393b5fa8fc7b45f291ee7998ee104186
HichamELMaa <hm.elmaataoui@gmail.com>
2023-05-04 10:05:52 -0700

    C - Bit manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * set_bit - Sets the value of a bit to 1 at a given index
- * @n: Pointer to the number to modify
- * @index: Index of the bit to set (starting from 0)
+ * set_bit - Sets the value of a bit to 1 at a given index.
+ * @n: A pointer to the number to be modified.
+ * @index: The index of the bit to be set.
  *
- * Return: 1 if successful, -1 if an error occurred
+ * Return: 1 if it worked, or -1 if an error occurred.
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-        if (index >= sizeof(unsigned long int) * 8)
-                return (-1);
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
 
-        *n |= (1 << index);
+	*n |= 1UL << index;
 
-        return (1);
+	return (1);
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,16 +9,16 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-        unsigned int count = 0;
-        unsigned long int flip = n ^ m;
+	unsigned int count = 0;
+	unsigned long int flip = n ^ m;
 
-        while (flip != 0)
-        {
-                if (flip & 1)
-                        count++;
+	while (flip != 0)
+	{
+		if (flip & 1)
+			count++;
 
-                flip = flip >> 1;
-        }
+		flip = flip >> 1;
+	}
 
-        return (count);
+	return (count);
 }
